b7f4d2e5f718e91fc3bb079eab40da5401b82b97
Annemarie <a.lucernoni0@gmail.com>
2023-08-29 16:24:41 -0600

    displaying quiz logo and text in column with general styling

--- lib/home_container.dart
+++ lib/home_container.dart
@@ -6,9 +6,25 @@ class HomeContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(color: Colors.deepPurple),
-      child: const Center(
-        child: Text('flutter app'),
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 66, 20, 146),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png', height: 300,),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 190, 123, 225), 
+                  fontSize: 24),
+            ),
+          ],
+        ),
       ),
     );
   }
