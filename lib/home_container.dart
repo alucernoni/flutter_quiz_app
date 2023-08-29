388bd197d3276a992ec2677545ba77cbd4da11a7
Annemarie <a.lucernoni0@gmail.com>
2023-08-29 16:30:51 -0600

    added elevated start quiz button

--- lib/home_container.dart
+++ lib/home_container.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomeContainer extends StatelessWidget {
   const HomeContainer({super.key});
 
+  startQuiz() {
+    // code to start quiz
+  }
+
   @override
   Widget build(context) {
     return Container(
@@ -23,6 +27,10 @@ class HomeContainer extends StatelessWidget {
                   color: Color.fromARGB(255, 190, 123, 225), 
                   fontSize: 24),
             ),
+            const SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: startQuiz(), 
+              child: const Text('Start Quiz', style: TextStyle(color: Color.fromARGB(255, 190, 123, 225),),),),
           ],
         ),
       ),
