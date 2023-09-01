e91faf34f132faeec422f05faa27d64a2e764e3a
Annemarie <a.lucernoni0@gmail.com>
2023-08-31 18:36:55 -0700

    building foundation for a switchscreen function to switch app from rendering home container vs question screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/home_container.dart';
+import 'package:quiz_app/question_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,11 +12,22 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const HomeContainer();
+  // don't use var because of it's strict type inference, it will only take type "HomeContainer", using Widget because accepting/flexible of any widget
+
+  switchScreen() {
+    setState(
+      () {
+        const QuestionScreen();
+      },
+    );
+  }
+
   @override
   Widget build(context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: HomeContainer(),
+        body: activeScreen,
       ),
     );
   }
