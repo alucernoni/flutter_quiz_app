aa46aa01a5b5dc20422f04db8277bd01d4dc9fcc
Annemarie <a.lucernoni0@gmail.com>
2023-08-31 18:30:17 -0700

    now importing quiz widget straight to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/home_container.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: HomeContainer(),
-      ),
-    ),
+   const Quiz(),
   );
 }
