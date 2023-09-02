43b6e58676e2809a041809bbda2d00b1417b8b21
Annemarie <a.lucernoni0@gmail.com>
2023-09-01 18:04:37 -0700

    question screen now dynamically accepts the question text and can get to the answers for display by using their indices in the list, not yet mapping

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -13,6 +14,8 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       // built into dart, is a way of centering horizontally
@@ -20,21 +23,26 @@ class _QuestionScreenState extends State<QuestionScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         // built in way of centering items in the column vertically
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.deepPurple),
+          Text(
+            currentQuestion.text,
+            //can access both text and answers properties through dot notation because we constructed our questions doc with the quizQuestion constructor that receives text, and answers arguments
+            style: const TextStyle(color: Colors.deepPurple),
           ),
           const SizedBox(height: 30),
           AnswerButton(
-            answerText: 'answer text...',
+            answerText: currentQuestion.answers[0],
             onClick: () {},
           ),
           AnswerButton(
-            answerText: 'answer text...',
+            answerText: currentQuestion.answers[1],
             onClick: () {},
           ),
           AnswerButton(
-            answerText: 'answer text...',
+            answerText: currentQuestion.answers[2],
+            onClick: () {},
+          ),
+          AnswerButton(
+            answerText: currentQuestion.answers[3],
             onClick: () {},
           ),
         ],
