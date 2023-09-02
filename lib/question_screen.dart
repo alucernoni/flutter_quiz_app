a7ebdf2114adc38bb3e6a085ba5efa56255580b3
Annemarie <a.lucernoni0@gmail.com>
2023-09-01 17:28:33 -0700

    new files for housing Q&A model and questions arrays, and updates to the UI for displaying the questions screen

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -10,9 +10,31 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
-  
   @override
   Widget build(context) {
-    return const Text('questions???');
+    return SizedBox(
+      width: double.infinity,
+      // built into dart, is a way of centering horizontally
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // built in way of centering items in the column vertically
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
