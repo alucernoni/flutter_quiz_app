6306df6ce12f526975d5e4918dc2a5d03e905ea4
Annemarie <a.lucernoni0@gmail.com>
2023-09-08 15:12:04 -0700

    styling quiz questions and text

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -19,33 +19,46 @@ class _QuestionScreenState extends State<QuestionScreen> {
     return SizedBox(
       width: double.infinity,
       // built into dart, is a way of centering horizontally
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        // built in way of centering items in the column vertically
-        children: [
-          Text(
-            currentQuestion.text,
-            //can access both text and answers properties through dot notation because we constructed our questions doc with the quizQuestion constructor that receives text, and answers arguments
-            style: const TextStyle(color: Colors.deepPurple),
-          ),
-          const SizedBox(height: 30),
-          AnswerButton(
-            answerText: currentQuestion.answers[0],
-            onClick: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[1],
-            onClick: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[2],
-            onClick: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[3],
-            onClick: () {},
-          ),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // built in way of centering items in the column vertically
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // built in way of expanding the items in the column to the full width of the column
+          children: [
+            Text(
+              textAlign: TextAlign.center,
+              currentQuestion.text,
+              //can access both text and answers properties through dot notation because we constructed our questions doc with the quizQuestion constructor that receives text, and answers arguments
+              style: const TextStyle(color: Colors.deepPurple),
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onClick: () {},
+              );
+            }),
+            // ... at the start of the mapping is how Flutter does the spread operator
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[0],
+            //   onClick: () {},
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[1],
+            //   onClick: () {},
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[2],
+            //   onClick: () {},
+            // ),
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[3],
+            //   onClick: () {},
+            // ),
+          ],
+        ),
       ),
     );
   }
